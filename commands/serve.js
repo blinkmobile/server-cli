5cc3a5aefef371ffcb67dce9bd01a298937c7be2
Matt Carroll <matt.carroll139@gmail.com>
2016-10-21 10:38:55 +1100

    SC-18 # Added cors config to serve cmd

--- commands/serve.js
+++ commands/serve.js
@@ -3,14 +3,17 @@
 const path = require('path')
 
 const serve = require('../lib/serve.js')
+const readCors = require('../lib/cors/read.js')
 
 module.exports = function (input, flags, logger, options) {
   const cwd = path.resolve(options.cwd)
   const example = path.join(cwd, 'helloworld', 'index.js')
-  return serve.startServer({
-    cwd,
-    port: 3000
-  })
+  return readCors(cwd)
+    .then((cors) => serve.startServer({
+      cors,
+      cwd,
+      port: 3000
+    })
     .then((server) => logger.log(`
 HTTP service for local development is running at port ${server.info.port}
 
@@ -22,5 +25,5 @@ HTTP API files should be in sub-folders within the directory above E.g:
 
 Create new HTTP APIs, rename, update, or delete them at any time
 Your changes automatically take effect on the next request
-`))
+`)))
 }
