986d7b60cfb0e936317a83ca50f187db4ca10343
Matt Carroll <matt.carroll139@gmail.com>
2016-10-25 14:43:55 +1100

    SC-25 # Added port flag to serve cmd

--- commands/serve.js
+++ commands/serve.js
@@ -2,28 +2,30 @@
 
 const path = require('path')
 
+const chalk = require('chalk')
+
 const serve = require('../lib/serve.js')
-const readCors = require('../lib/cors/read.js')
 
 module.exports = function (input, flags, logger, options) {
   const cwd = path.resolve(options.cwd)
-  const example = path.join(cwd, 'helloworld', 'index.js')
-  return readCors(cwd)
-    .then((cors) => serve.startServer({
-      cors,
-      cwd,
-      port: 3000
-    })
-    .then((server) => logger.log(`
-HTTP service for local development is running at port ${server.info.port}
+  const example = path.join('helloworld', 'index.js')
+  return serve.startServer({
+    cwd,
+    port: flags.port || 3000
+  })
+  .then((server) => logger.log(`
+HTTP service for local development is available from:
+  ${server.info.uri}
 
 Your current directory "." is:
   ${cwd}
 
 HTTP API files should be in sub-folders within the directory above E.g:
-  ${example}
+  ${example} -> ${server.info.uri}/helloworld
 
-Create new HTTP APIs, rename, update, or delete them at any time
+Create new HTTP APIs, rename, update, or delete them at any time.
 Your changes automatically take effect on the next request
-`)))
+
+${chalk.yellow('Hit CTRL-C to stop the service')}
+`))
 }
