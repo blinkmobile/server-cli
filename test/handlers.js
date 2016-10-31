a80cf85a134310bda4788642e3888652991aa569
Matt Carroll <matt.carroll139@gmail.com>
2016-10-31 14:42:12 +1100

    SC-19 # Updated handler configuration for better error handling

--- test/handlers.js
+++ test/handlers.js
@@ -20,18 +20,28 @@ test('executeHandler()', (t) => {
     .then(() => t.truthy(isHandlerCalled))
 })
 
-test('getHandler()', (t) => {
+test('getHandler() valid modules', (t) => {
   const tests = [
-    { args: [ path.join(EXAMPLE_DIR, 'missing'), 'get' ], expected: 'undefined' },
     { args: [ path.join(EXAMPLE_DIR, 'helloworld'), 'get' ], expected: 'function' },
     { args: [ path.join(EXAMPLE_DIR, 'methods'), 'get' ], expected: 'function' },
     { args: [ path.join(EXAMPLE_DIR, 'methods'), 'patch' ], expected: 'object' },
-    { args: [ path.join(CONFIGURATION_DIR, 'api/missing'), 'get' ], expected: 'undefined' },
     { args: [ path.join(CONFIGURATION_DIR, 'api/request'), 'get' ], expected: 'function' },
     { args: [ path.join(CONFIGURATION_DIR, 'api/books'), 'get' ], expected: 'function' },
     { args: [ path.join(CONFIGURATION_DIR, 'api/books'), 'patch' ], expected: 'object' }
   ]
-  tests.forEach(({ args, expected }) => {
-    t.is(typeof lib.getHandler(...args), expected)
-  })
+  return tests.reduce((prev, config) => {
+    return prev.then(() => lib.getHandler(...config.args))
+      .then(result => t.is(typeof result, config.expected))
+  }, Promise.resolve())
+})
+
+test('getHandler() invalid modules', (t) => {
+  const tests = [
+    { args: [ path.join(EXAMPLE_DIR, 'missing'), 'get' ], expected: `Cannot find module '${path.join(EXAMPLE_DIR, 'missing')}'` },
+    { args: [ path.join(CONFIGURATION_DIR, 'api/missing'), 'get' ], expected: `Cannot find module '${path.join(CONFIGURATION_DIR, 'api/missing')}'` }
+  ]
+
+  return tests.reduce((prev, config) => {
+    return prev.then(() => t.throws(lib.getHandler(...config.args), config.expected))
+  }, Promise.resolve())
 })
