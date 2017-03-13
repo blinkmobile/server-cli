3a482abdd982b6611ce59cd0865d94b78189aaad
Matt Carroll <matt.carroll139@gmail.com>
2017-03-13 14:44:59 +1100

    SC-57 # function for reading single route config

---
+++ test/routes/get-route-config.js
@@ -0,0 +1,44 @@
+/* @flow */
+'use strict'
+
+const test = require('ava')
+const proxyquire = require('proxyquire')
+
+const TEST_SUBJECT = '../../lib/routes/get-route-config.js'
+
+const CWD = 'current working directory'
+const ROUTES = [
+  {
+    'route': '/',
+    'module': './api/root.js'
+  },
+  {
+    'route': '/api/books/:id',
+    'module': './api/book.js'
+  },
+  {
+    'route': '/api/books',
+    'module': 'api/books.js'
+  }
+]
+
+function getTestSubject (overrides) {
+  overrides = overrides || {}
+  return proxyquire(TEST_SUBJECT, Object.assign({
+    './read.js': () => Promise.resolve(ROUTES)
+  }, overrides))
+}
+
+test('should return route config', (t) => {
+  const lib = getTestSubject()
+  return lib(CWD, '/api/books/:id')
+    .then((routeConfig) => t.deepEqual(routeConfig, ROUTES[1]))
+})
+
+test('should reject if a project does not contain route', (t) => {
+  const lib = getTestSubject()
+  t.throws(
+    lib(CWD, '/route'),
+    'Project does not contain route: /route'
+  )
+})
