13c25a7b11e82140276594257c8cb7cf4314b5cb
Matt Carroll <matt.carroll139@gmail.com>
2019-05-02 15:01:20 +1000

    ON-4052 # Removed ignoring errors in element lookup example

--- examples/lookup/src/company-assets.js
+++ examples/lookup/src/company-assets.js
@@ -39,7 +39,7 @@ module.exports.post = function (req, res) {
     return res
       .setStatusCode(400)
       .setPayload({
-        'message': 'This is my custom friendly error message that will be shown to the user on a failed lookup'
+        message: 'This is my custom friendly error message that will be shown to the user on a failed lookup'
       })
   }
 
@@ -47,47 +47,40 @@ module.exports.post = function (req, res) {
   const warehouseNumber = req.body.submission.warehouseNumber
   const warehouse = warehouses.find(warehouse => warehouse.warehouseNumber === warehouseNumber)
 
-  if (warehouse && warehouse.assets) {
-    // Loop through all assets and create an element for each using the SDK.
-    const elements = []
-    const assets = warehouse.assets
-    
-    for (const asset of assets) {
-      // Configure the element data as a basic yes/no set of radio buttons using the asset name in the label.
-      const elementData = {
-        name: asset,
-        label: `Is the ${asset} present in the warehouse?`,
-        type: 'radio',
-        buttons: true,
-        options: [
-          {
-            value: 'yes',
-            label: 'yes'
-          },
-          {
-            value: 'no',
-            label: 'no'
-          }
-        ]
-      }
-      try {
-        // Send the above basic data to the SDK to generate a form element.
-        const element = OneBlink.Forms.generateFormElement(elementData)
-        elements.push(element)
-      } catch (error) {
-        // Silently fail on any validation errors so other elements in the loop aren't affected.
-      }
-    }
-
-    // Set the response code and set body as the array of form elements.
+  // If the request does not contain a valid warehouse number,
+  // finish early with a custom error message for the user to see.
+  if (!warehouse) {
     return res
-      .setStatusCode(200)
-      .setPayload(elements)
+      .setStatusCode(400)
+      .setPayload({
+        message: 'Could not find the warehouse you were looking for.'
+      })
   }
 
-  return res
-    .setStatusCode(400)
-    .setPayload({
-      'message': 'Could not find the warehouse number you were looking for.'
+  // Loop through all assets and create an element for each using the SDK.
+  const elements = warehouse.assets.map((asset) => {
+    return OneBlink.Forms.generateFormElement({
+      name: asset,
+      label: `Is the ${asset} present in the warehouse?`,
+      type: 'radio',
+      buttons: true,
+      required: true,
+      options: [
+        {
+          value: 'yes',
+          label: 'Yes',
+          colour: '#43a047' // green
+        },
+        {
+          value: 'no',
+          label: 'No',
+          colour: '#f44336' // red
+        }
+      ]
     })
+  })
+
+  return res
+    .setStatusCode(200)
+    .setPayload(elements)
 }
